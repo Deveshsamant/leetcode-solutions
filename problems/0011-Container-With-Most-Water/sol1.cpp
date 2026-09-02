610332d44d76cfbba32eb93c16313d34e9f1b577
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-02 20:01:23 +0530

    Add sol1: 11. Container With Most Water (C++)

---
+++ problems/0011-Container-With-Most-Water/sol1.cpp
@@ -0,0 +1,11 @@
+// ==========================================================
+// 11. Container With Most Water
+// Difficulty : Medium
+// Language   : C++
+// Solution   : #1
+// Runtime    : 0 ms (Beats 100%)
+// Memory     : 62.9 MB (Beats 49%)
+// Link       : https://leetcode.com/problems/container-with-most-water/
+// ==========================================================
+
+ 
