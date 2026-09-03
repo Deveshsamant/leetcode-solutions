3bf1c139d2187a98905f9684a66d29b90115ea5f
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-03 17:05:59 +0530

    Add sol1: 41. First Missing Positive (C++)

---
+++ problems/0041-First-Missing-Positive/sol1.cpp
@@ -0,0 +1,11 @@
+// ==========================================================
+// 41. First Missing Positive
+// Difficulty : Hard
+// Language   : C++
+// Solution   : #1
+// Runtime    : 111 ms (Beats 5%)
+// Memory     : 84.4 MB (Beats 5%)
+// Link       : https://leetcode.com/problems/first-missing-positive/
+// ==========================================================
+
+ 
