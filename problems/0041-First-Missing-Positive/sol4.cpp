df04567ae79ecffe65c6d544750c9c2de7d41ca3
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-03 18:34:56 +0530

    Add sol4: 41. First Missing Positive (C++)

---
+++ problems/0041-First-Missing-Positive/sol4.cpp
@@ -0,0 +1,23 @@
+// ==========================================================
+// 41. First Missing Positive
+// Difficulty : Hard
+// Language   : C++
+// Solution   : #4
+// Runtime    : 97 ms (Beats 5%)
+// Memory     : 84.4 MB (Beats 5%)
+// Link       : https://leetcode.com/problems/first-missing-positive/
+// ==========================================================
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        vector<int> x;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                x.push_back(nums[i]);
+            }
+        }
+        sort(x.begin(),x.end());
+        set<int> y;
+        y.insert(x.begin(),x.end());
+        x.clear();
