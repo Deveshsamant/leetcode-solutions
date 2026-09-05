4b6fe164b6fbb961f1c8d5935a5fb709753a5ec8
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-05 20:17:35 +0530

    Add sol1: 73. Set Matrix Zeroes (Java)

---
+++ problems/0073-Set-Matrix-Zeroes/sol1.java
@@ -0,0 +1,11 @@
+// ==========================================================
+// 73. Set Matrix Zeroes
+// Difficulty : Medium
+// Language   : Java
+// Solution   : #1
+// Runtime    : 1 ms (Beats 97%)
+// Memory     : 47.6 MB (Beats 42%)
+// Link       : https://leetcode.com/problems/set-matrix-zeroes/
+// ==========================================================
+
+ 
