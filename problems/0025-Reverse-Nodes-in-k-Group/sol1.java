09d921cfb1b5ca20ee3352aba6bf5313ed32690e
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-06 21:40:44 +0530

    Add sol1: 25. Reverse Nodes in k-Group (Java)

---
+++ problems/0025-Reverse-Nodes-in-k-Group/sol1.java
@@ -0,0 +1,33 @@
+// ==========================================================
+// 25. Reverse Nodes in k-Group
+// Difficulty : Hard
+// Language   : Java
+// Solution   : #1
+// Runtime    : 0 ms (Beats 100%)
+// Memory     : 46.3 MB (Beats 55%)
+// Link       : https://leetcode.com/problems/reverse-nodes-in-k-group/
+// ==========================================================
+
+            } else {
+                newHead = newGroupHead;                
+            if (prevTail == null) {
+
+            ListNode newGroupHead = reversesec(x, k, nextit);
+            for (int y = 0; y < k; y++) nextit = nextit.next;
+
+            ListNode nextit = x;
+        while (gg != 0) {
+
+        ListNode prevTail = null;    
+        ListNode newHead = head;     
+        ListNode x = head;          
+        int gg = c / k;
+
+            ListNode oldX = x;                          
+        for (ListNode t = head; t != null; t = t.next) c++;
+        int c = 0;
+        if (head == null || k == 1) return head;
+    public ListNode reverseKGroup(ListNode head, int k) {
+class Solution {
+                prevTail.next = newGroupHead;            
+            }
