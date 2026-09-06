f919f2a778ecdf9970814a51a72e12fa433627af
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-06 19:09:13 +0530

    Add sol1: 160. Intersection of Two Linked Lists (Java)

---
+++ problems/0160-Intersection-of-Two-Linked-Lists/sol1.java
@@ -0,0 +1,23 @@
+// ==========================================================
+// 160. Intersection of Two Linked Lists
+// Difficulty : Easy
+// Language   : Java
+// Solution   : #1
+// Runtime    : 1 ms (Beats 100%)
+// Memory     : 52.8 MB (Beats 45%)
+// Link       : https://leetcode.com/problems/intersection-of-two-linked-lists/
+// ==========================================================
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+ * }
+ */
+public class Solution {
+ *     }
+ *         next = null;
+ *     ListNode(int x) {
+ *         val = x;
+            int countA = 0;
+        int countB = 0;
+
+        ListNode tempA = headA;
+        ListNode tempB = headB;
