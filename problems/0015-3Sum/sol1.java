9d1bf7ac06ab8bb569d2a9f8171dfeabfd31d198
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-07 15:40:52 +0530

    Add sol1: 15. 3Sum (Java)

---
+++ problems/0015-3Sum/sol1.java
@@ -0,0 +1,25 @@
+// ==========================================================
+// 15. 3Sum
+// Difficulty : Medium
+// Language   : Java
+// Solution   : #1
+// Runtime    : 33 ms (Beats 71%)
+// Memory     : 60.5 MB (Beats 5%)
+// Link       : https://leetcode.com/problems/3sum/
+// ==========================================================
+
+                    temp.add(nums[i]);
+                    List<Integer> temp=new ArrayList<>();
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0){
+            while(l<r){
+            }
+            int r=nums.length-1;
+            if(i > 0 &&  nums[i]==nums[i-1]){
+                continue;
+        for(int i=0;i<nums.length-2;i++){
+            int l=i+1;
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+class Solution {
